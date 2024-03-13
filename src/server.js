0c779cf3db677b47a81054b9747d753f37803927
Your Name <you@example.com>
2024-03-13 22:52:50 +0700

    docs: code base back-end

--- src/server.js
+++ src/server.js
@@ -1,13 +1,27 @@
-const express = require('express')
+import express from 'express'
+import { mapOrder } from '~/utils/sorts.js'
+
 const app = express()
 
 const hostname = 'localhost'
-const port = '8017'
+const port = 8017
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World NodeJS TranTung31!</h1>')
+  // Test Absolute import mapOrder
+  // eslint-disable-next-line no-console
+  console.log(mapOrder(
+    [{ id: 'id-1', name: 'One' },
+      { id: 'id-2', name: 'Two' },
+      { id: 'id-3', name: 'Three' },
+      { id: 'id-4', name: 'Four' },
+      { id: 'id-5', name: 'Five' }],
+    ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
+    'id'
+  ))
+  res.end('<h1>Hello World!</h1><hr>')
 })
 
 app.listen(port, hostname, () => {
-  console.log(`Hello TranTung31, I'am running server at http://${hostname}:${port}/`);
+  // eslint-disable-next-line no-console
+  console.log(`Hello TranTungDev, I am running at http://${ hostname }:${ port }`)
 })
