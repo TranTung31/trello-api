a09323dd71abec21c3b5c4008b982aaa82fe01e1
Your Name <you@example.com>
2024-03-14 17:23:33 +0700

    feat: config env

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,14 +1,11 @@
-const MONGODB_URI = 'mongodb+srv://trantung310502:k7K4yzLqfAG4emVV@cluster0-trantungdev.tk7wb73.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0-TranTungDev'
-
-const DATABASE_NAME = 'trello-trantungdev'
-
 import { MongoClient, ServerApiVersion } from 'mongodb'
+import { env } from '~/config/environment'
 
 // Khởi tạo 1 đối tượng trelloDatabaseInstance ban đầu là null (vì chúng ta chưa connect)
 let trelloDatabaseInstance = null
 
 // Khởi tạo 1 đối tượng mongoClientInstance để connect tới MongoDB
-const mongoClientInstance = new MongoClient(MONGODB_URI, {
+const mongoClientInstance = new MongoClient(env.MONGODB_URI, {
   // Lưu ý: serverApi có từ phiên bản MongoDB 5.0.0 trở lên, có thể không cần dùng nó, còn nếu dùng nó
   // chúng ta sẽ chỉ định 1 cái Stable API Version của MongoDB
   serverApi: {
@@ -24,7 +21,7 @@ export const CONNECT_DB = async () => {
   await mongoClientInstance.connect()
 
   // Kết nối thành công thì lấy ra Database theo tên và gán ngược lại vào biến trelloDatabaseInstance
-  trelloDatabaseInstance = mongoClientInstance.db(DATABASE_NAME)
+  trelloDatabaseInstance = mongoClientInstance.db(env.DATABASE_NAME)
 }
 
 // Function GET_DB() này có nhiệm vụ export ra trelloDatabaseInstance sau khi đã connect thành công tới MongoDB
--- src/server.js
+++ src/server.js
@@ -1,21 +1,18 @@
 /* eslint-disable no-console */
 import express from 'express'
 import exitHook from 'async-exit-hook'
-import { CONNECT_DB, GET_DB, CLOSE_DB } from '~/config/mongodb'
+import { CONNECT_DB, CLOSE_DB } from '~/config/mongodb'
+import { env } from '~/config/environment'
 
 const START_SERVER = () => {
   const app = express()
 
-  const hostname = 'localhost'
-  const port = 8017
-
   app.get('/', async (req, res) => {
-    // console.log(await GET_DB().listCollections().toArray())
     res.end('<h1>Hello World!</h1>')
   })
 
-  app.listen(port, hostname, () => {
-    console.log(`3. Hello TranTungDev, Back-end Server is running at http://${ hostname }:${ port }`)
+  app.listen(env.APP_PORT, env.APP_HOST, () => {
+    console.log(`3. Hello ${env.AUTHOR}, Back-end Server is running at http://${env.APP_HOST}:${env.APP_PORT}`)
   })
 
   exitHook(() => {
