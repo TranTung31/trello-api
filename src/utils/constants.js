23707ee419d6b680748efc1d88e7698bf482dbd6
Your Name <you@example.com>
2024-03-24 00:12:57 +0700

    fix: update arr whitelist domains

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,6 @@
 // Những domain được phép truy cập tới tài nguyên của Server
 export const WHITELIST_DOMAINS = [
+  'https://trello-web-chi-five.vercel.app'
   // 'http://localhost:5173' Không cần localhost nữa vì ở file config/cors đã luôn cho phép môi trường dev
 ]
 
